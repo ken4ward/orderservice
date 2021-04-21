2796e721e92b92819701e89ae819add737e73707
Kehinde Adeoya <ken4ward@yahoo.com>
2021-04-21 20:54:04 +0100

    Initial commit

---
+++ src/main/java/com/christdoes/orderservice/orderservice/OrderserviceApplication.java
@@ -0,0 +1,13 @@
+package com.christdoes.orderservice.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserviceApplication.class, args);
+	}
+}
